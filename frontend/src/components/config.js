d0a89b5ffed95e12efcd9d2906f5b656977a7312
programmermesh <pstmeshach@gmail.com>
2026-02-22 00:25:17 +0100

    uppdated the config.js

--- frontend/src/components/config.js
+++ frontend/src/components/config.js
@@ -1,1 +1,2 @@
-export const API_URL = "http://localhost:3000/attendance";
+//export const API_URL = "http://localhost:3000/attendance";
+export const API_URL = "https://uni-attendance.onrender.com/attendance";
